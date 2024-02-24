a42d49fbad348ba7569f59cd91d2c73b3b6a14aa
seunghunbang <upi21x@gmail.com>
2024-02-24 20:58:45 +0900

    Change BE resources

--- backend/src/main/java/com/example/React_J_Spring_Todo_test/ReactJSpringTodoTestApplication.java
+++ backend/src/main/java/com/example/React_J_Spring_Todo_test/ReactJSpringTodoTestApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.React_J_Spring_Todo_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTodoApplication {
+public class ReactJSpringTodoTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTodoApplication.class, args);
+		SpringApplication.run(ReactJSpringTodoTestApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/React_J_Spring_Todo_test/ReactJSpringTodoTestApplicationTests.java
+++ backend/src/test/java/com/example/React_J_Spring_Todo_test/ReactJSpringTodoTestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.React_J_Spring_Todo_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringTodoApplicationTests {
+class ReactJSpringTodoTestApplicationTests {
 
 	@Test
 	void contextLoads() {
